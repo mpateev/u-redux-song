147e6af178ab7dd2257250f13d70717e1d29df71
murad.pateev <mpateev@gmail.com>
2021-01-25 15:11:05 +0100

    redux

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -2,11 +2,16 @@ import React from "react";
 import { connect } from "react-redux";
 
 const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a Song!</div>;
+  }
   return (
     <div>
       <h2>{song && "Song Detail"}</h2>
-      <h3>{song && "Song Title: " + song.title}</h3>
-      <h4>{song && "Duration: " + song.duration}</h4>
+      <h3>Title</h3>
+      <p>{song.title}</p>
+      <h3>Duration:</h3>
+      <p>{song.duration}</p>
     </div>
   );
 };
